c70b958336149dbedc852d80e83e5eb7d262bf93
Your Name <you@example.com>
2024-06-20 10:01:28 +0700

    membuat form

--- src/component/CONTACT/form/form.jsx
+++ src/component/CONTACT/form/form.jsx
@@ -1,4 +1,19 @@
+import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
+import { useRef } from "react";
+import Swal from "sweetalert2";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    name: yup.string().required().max(25).min(4),
+    email: yup.string().email().required(),
+    message: yup.string().required().max(500),
+  })
+  .required();
+
 const Form = () => {
   const variant = {
     hidden: {
@@ -12,9 +27,46 @@ const Form = () => {
       x: 0,
     },
   };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema) });
+
+  const form = useRef();
+
+  const sendEmail = () => {
+    emailjs
+      .sendForm("service_kj2o1hf", "template_idtsf97", form.current, {
+        publicKey: "-QKPLhdZfJHcyu4KX",
+      })
+      .then(
+        () => {
+          Swal.fire({
+            title: "Thank You",
+            text: "Message Send",
+            icon: "success",
+          });
+        },
+        () => {
+          Swal.fire({
+            title: "upps",
+            text: "something error",
+            icon: "error",
+          });
+        }
+      );
+  };
+
   return (
     <>
-      <form className="w-[60%] mx-auto text-sm text-black" action="">
+      <form
+        className="w-[60%] mx-auto text-sm text-black"
+        ref={form}
+        onSubmit={handleSubmit(sendEmail)}
+        action=""
+      >
         <div
           className="flex justify-center items-center gap-4 text-xl"
           id="name-email"
@@ -34,9 +86,12 @@ const Form = () => {
               whileInView="visible"
               transition={{ duration: 0.5 }}
               type="text"
-              name="text"
+              name="name"
               id=""
+              required
+              {...register("name", { required: true })}
             />
+            {errors?.name && <p>{errors.name?.message}</p>}
           </div>
           <div className="w-1/2">
             <motion.label
@@ -55,7 +110,10 @@ const Form = () => {
               type="email"
               name="email"
               id=""
+              required
+              {...register("email", { required: true })}
             />
+            {errors?.email && <p>{errors.email?.message}</p>}
           </div>
         </div>
 
@@ -74,21 +132,26 @@ const Form = () => {
             whileInView="visible"
             transition={{ duration: 0.5, delay: 0.8 }}
             type="text"
-            name="text"
+            name="message"
             className="w-full"
             id=""
+            required
+            {...register("message", { required: true })}
           />
+          {errors?.message && <p>{errors.message?.message}</p>}
         </div>
+        <motion.button
+          variants={variant}
+          initial="hidden"
+          animate="visible"
+          transition={{ duration: 1 }}
+          className="rounded-lg bg-second text-treed px-4 py-2 text-sm mt-2"
+          value="Send"
+          type="submit"
+        >
+          Download CV
+        </motion.button>
       </form>
-      <motion.button
-        variants={variant}
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 1 }}
-        className="rounded-lg bg-second text-treed px-4 py-2 text-sm mt-2"
-      >
-        Download CV
-      </motion.button>
     </>
   );
 };
